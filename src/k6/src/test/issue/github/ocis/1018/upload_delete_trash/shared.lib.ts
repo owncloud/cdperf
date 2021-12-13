6250dd4890c320c266c2f22a01ed76e610589591
Rob Pocklington <rpocklin@gmail.com>
2021-12-13 14:26:26 +1100

    Add trash and restore tests with large and small files

---
+++ src/k6/src/test/issue/github/ocis/1018/upload_delete_trash/shared.lib.ts
@@ -0,0 +1,80 @@
+import { b64decode } from 'k6/encoding';
+import { Options, Threshold } from 'k6/options';
+
+import { playbook, types, utils } from '../../../../../../lib';
+
+interface File {
+    size: number;
+    unit: types.AssetUnit;
+}
+
+interface Plays {
+    davUpload: playbook.dav.Upload;
+    davDelete: playbook.dav.Delete;
+    davTrash: playbook.dav.Trash;
+}
+
+export const options = ({ files, plays }: { files: File[]; plays: Plays }): Options => {
+    return {
+        thresholds: files.reduce((acc: { [name: string]: Threshold[] }, c) => {
+            acc[`${plays.davUpload.metricTrendName}{asset:${c.unit + c.size.toString()}}`] = [];
+            acc[`${plays.davDelete.metricTrendName}{asset:${c.unit + c.size.toString()}}`] = [];
+            acc[`${plays.davTrash.metricTrendName}{asset:${c.unit + c.size.toString()}}`] = [];
+            return acc;
+        }, {}),
+    };
+};
+
+export default ({
+    files,
+    account,
+    credential,
+    plays,
+}: {
+    plays: Plays;
+    files: File[];
+    account: types.Account;
+    credential: types.Credential;
+}): void => {
+    const filesUploaded: { id: string; name: string; fileid: string }[] = [];
+
+    files.forEach((f) => {
+        const id = f.unit + f.size.toString();
+
+        const asset = utils.buildAsset({
+            name: `${account.login}-dummy.zip`,
+            unit: f.unit,
+            size: f.size,
+        });
+
+        const output = plays.davUpload.exec({
+            credential,
+            asset,
+            userName: account.login,
+            tags: { asset: id },
+        });
+
+        const base64Fileid = output.response.headers['Oc-Fileid'];
+        const fileid = b64decode(base64Fileid, 'std', 's').split(':')[1];
+
+        filesUploaded.push({ id, name: asset.name, fileid: fileid });
+    });
+
+    filesUploaded.forEach((f) => {
+        plays.davDelete.exec({
+            credential,
+            userName: account.login,
+            path: f.name,
+            tags: { asset: f.id },
+        });
+    });
+
+    filesUploaded.forEach((f) => {
+        plays.davTrash.exec({
+            credential,
+            userName: account.login,
+            fileid: f.fileid,
+            tags: { asset: f.id },
+        });
+    });
+};
