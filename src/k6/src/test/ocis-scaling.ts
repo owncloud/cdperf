db8691bf29c4f3092600f1ff3999f726adf9da99
Christian Richter <crichter@owncloud.com>
2022-03-22 16:23:58 +0100

    Add random user creation
    Co-authored-by Willy Kloucek <wkloucek@owncloud.com>
    Signed-off-by: Christian Richter <crichter@owncloud.com>

--- src/k6/src/test/ocis-scaling.ts
+++ src/k6/src/test/ocis-scaling.ts
@@ -1,19 +1,21 @@
-import { Options } from 'k6/options';
+//import { Options } from 'k6/options';
 
 import { times } from 'lodash';
 
-import { auth, defaults, k6, playbook, types, utils } from  '../lib';
-import http from 'k6/http';
+import { auth, defaults, playbook, types, utils } from  '../lib';
+//import http from 'k6/http';
 
 import { sleep } from 'k6';
 
-import exec from 'k6/execution';
+//import exec from 'k6/execution';
+
+import { Create, Delete } from '../lib/api/users';
 
 const file: {
   size: number;
   unit: types.AssetUnit;
 } = {
-  size: 10,
+  size: 1024,
   unit: 'KB',
 };
 
@@ -26,7 +28,53 @@ const plays = {
     davPropfind: new playbook.dav.Propfind(),
 };
 
+const DefaultPassword = "foobar";
+const TargetBaseVUS = 10
+const TargetMaxVUS = 100  // NOTE: this also defines the number of user accounts created
+const AccountPrefix = "user"
+const TearDownCoolDownTime = 60
+
+export function setup (): void {
+  console.log("Generating ".concat(TargetMaxVUS.toString()).concat(" users"))
+  for(var i=0;i<TargetMaxVUS; i++){
+    var res = Create.exec({
+      userName: AccountPrefix.concat(i.toString()),
+      password: DefaultPassword,
+      email: AccountPrefix.concat(i.toString()).concat("@example.com"),
+      credential: adminAuthFactory.credential
+    })
+    if(res.status != 200) {
+      console.log(res.status.toString().concat(" ").concat(res.error))
+    }
+    if(i%50==0) {
+      sleep(1);
+    }
+  }
+  console.log("done")
+}
+
+export function teardown (): void {
+  sleep(TearDownCoolDownTime)
+  console.log("Deleting ".concat(TargetMaxVUS.toString()).concat(" users"))
+  for(var i=0;i<TargetMaxVUS; i++){
+    var res = Delete.exec({
+      userName: AccountPrefix.concat(i.toString()),
+      credential: adminAuthFactory.credential
+    })
+    if(res.status != 200) {
+      console.log(
+        res.status.toString().concat(" ").concat(res.error).concat(" for ").concat(AccountPrefix).concat(i.toString()))
+    }
+    if(i%50==0) {
+      sleep(1);
+    }
+  }
+  console.log("done")
+} 
+
 export const options = {
+    setupTimeout: (TargetMaxVUS*2).toString().concat('s'),
+    teardownTimeout: (TargetMaxVUS*2+TearDownCoolDownTime).toString().concat('s'),
     tags: {
       test_id: 'kube-cluster-loadtesting',
     },
@@ -40,14 +88,14 @@ export const options = {
           executor: 'ramping-vus',
           startVUs: 0,
           stages: [
-            { duration: '60s', target: 100 },
+            { duration: '60s', target: TargetBaseVUS },
           ],
           gracefulRampDown: '0s',
           tags: { metric_tag: 'warmup'},
         },
         run: {
           executor: 'constant-vus',
-          vus: 100,
+          vus: TargetBaseVUS,
           startTime: '60s',
           duration: '60s',
           tags: { metric_tag: 'run'},
@@ -57,14 +105,14 @@ export const options = {
           startVUs: 0,
           startTime: '120s',
           stages: [
-            { duration: '60s', target: 1000 },
+            { duration: '60s', target: TargetMaxVUS },
           ],
           gracefulRampDown: '0s',
           tags: { metric_tag: 'rampup'},
         },
         stress: {
           executor: 'constant-vus',
-          vus: 1000,
+          vus: TargetMaxVUS,
           startTime: '180s',
           duration: '60s',
           tags: { metric_tag: 'stress'},
@@ -72,16 +120,16 @@ export const options = {
         recover: {
           executor: 'ramping-vus',
           startTime: '240s',
-          startVUs: 1000,
+          startVUs: TargetMaxVUS,
           stages: [
-            { duration: '60s', target: 100 },
+            { duration: '60s', target: TargetBaseVUS },
           ],
           gracefulRampDown: '30s',
           tags: { metric_tag: 'recover'},
         },
         normalize: {
           executor: 'constant-vus',
-          vus: 100,
+          vus: TargetBaseVUS,
           startTime: '300s',
           duration: '60s',
           tags: { metric_tag: 'normalize'},
@@ -90,19 +138,31 @@ export const options = {
 };
 
 export default (): void => {
-      const admin = {
-        name: adminAuthFactory.account.login,
-        password: adminAuthFactory.account.password,
-        credential: adminAuthFactory.credential,
-    };
+    //const loggedInUser = {
+    //    name: adminAuthFactory.account.login,
+    //    password: adminAuthFactory.account.password,
+    //    credential: adminAuthFactory.credential,
+    //};
 
+    // using modulo here, k6 counts up the VU numbers for each process run in each scenario,
+    // resulting in ids > TargetMaxVUS
+    const userName = AccountPrefix.concat((__VU%TargetMaxVUS).toString())
+    const loggedInUser = {
+      name:  userName,
+      password: DefaultPassword,
+      credential: new auth({
+                    login: userName,
+                    password: DefaultPassword
+                  }).credential
+    };
+    
     const folders = times(50, (i) => `VU-${__VU}-ITER-${__ITER}-I-${i}`).reduce((acc: string[][], tlf, i) => {
       acc.push([tlf, ...times(5, (i) => `D-${i + 1}`)]);
       acc[i].forEach((_, ci) => {
           plays.davCreate.exec({
-              credential: admin.credential,
+              credential: loggedInUser.credential,
               path: acc[i].slice(0, ci + 1).join('/'),
-              userName: admin.name,
+              userName: loggedInUser.name,
           });
       });
       return acc;
@@ -117,8 +177,8 @@ export default (): void => {
         g.forEach((_, i) => {
             plays.davUpload.exec({
                 asset: asset,
-                credential: admin.credential,
-                userName: admin.name,
+                credential: loggedInUser.credential,
+                userName: loggedInUser.name,
                 path: g.slice(0, i + 1).join('/'),
             });
         });
@@ -131,15 +191,15 @@ export default (): void => {
             const destination = [...g.slice(0, g.length - i - 1), newName].join('/');
 
             plays.davMove.exec({
-                credential: admin.credential,
-                userName: admin.name,
+                credential: loggedInUser.credential,
+                userName: loggedInUser.name,
                 path,
                 destination,
             });
 
             plays.davPropfind.exec({
-                credential: admin.credential,
-                userName: admin.name,
+                credential: loggedInUser.credential,
+                userName: loggedInUser.name,
                 path: destination,
             });
 
@@ -149,12 +209,9 @@ export default (): void => {
 
     folders.forEach((g) => {
         plays.davDelete.exec({
-            credential: admin.credential,
-            userName: admin.name,
+            credential: loggedInUser.credential,
+            userName: loggedInUser.name,
             path: g[0],
         });
     });
-    //console.log(exec.vu.idInTest);
-    //http.get('https://ocis.ocis-kube.owncloud.works')
-    //sleep(1)
 }
